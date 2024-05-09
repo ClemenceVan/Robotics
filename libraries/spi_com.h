44e07b4a6ce09e910ebab628e459bba7cd0ea85b
ClemenceVan <clemence.vanpouille@epitech.eu>
2024-05-09 22:22:36 +0200

    Fixed typo

--- libraries/spi_com.h
+++ libraries/spi_com.h
@@ -1,25 +1,24 @@
 #ifndef SPI_COM_H
-#define SPI_COM_H
+	#define SPI_COM_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+	#ifdef __cplusplus
+		extern "C" {
+	#endif
 
-typedef struct{
+	typedef struct{
 
-	signed Set_Speed_M1 :16;
-	signed Set_Speed_M2 :16;
-	signed Act_Speed_M1 :16;
-	signed Act_Speed_M2 :16;
-	signed Encoder_M1 :32;
-	signed Encoder_M2 :32;                                                                            
-} MotorDataType;
+		signed Set_Speed_M1 :16;
+		signed Set_Speed_M2 :16;
+		signed Act_Speed_M1 :16;
+		signed Act_Speed_M2 :16;
+		signed Encoder_M1 :32;
+		signed Encoder_M2 :32;                                                                            
+	} MotorDataType;
 
 
-void Send_Read_Motor_Data(MotorDataType *);
+	void Send_Read_Motor_Data(MotorDataType *);
 
-#endif
-
-#ifdef __cplusplus
-}
+	#ifdef __cplusplus
+		}
+	#endif
 #endif
